f2b429681a32b6f4966984f87578d710e2b75813
a2175 <as217512@gmail.com>
2024-11-02 17:46:36 +0900

    init

---
+++ src/main/java/hello/springoauth2client/Springoauth2clientApplication.java
@@ -0,0 +1,13 @@
+package hello.springoauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springoauth2clientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springoauth2clientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springoauth2client/Springoauth2clientApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springoauth2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springoauth2clientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
