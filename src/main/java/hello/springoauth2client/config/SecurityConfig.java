6fc8e4ed3b6ebaca6cca7fa75d63003428497aea
a2175 <as217512@gmail.com>
2024-11-09 16:09:01 +0900

    스프링 OAuth2 클라이언트 세션 12 : Client Registration
    https://www.youtube.com/watch?v=cn3a8b2Gt8k&list=PLJkjrxxiBSFBGk0b931ZkCVlNUo7sFisu&index=13

--- src/main/java/hello/springoauth2client/config/SecurityConfig.java
+++ src/main/java/hello/springoauth2client/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package hello.springoauth2client.config;
 
+import hello.springoauth2client.oauth2.CustomClientRegistrationRepo;
 import hello.springoauth2client.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomClientRegistrationRepo customClientRegistrationRepo;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,6 +33,7 @@ public class SecurityConfig {
         http
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
+                        .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))
                 );
